d5afe67058b49ea8fae13d64479d6e69abc53d1d
Lukh01 <akimforever19@gmail.com>
2024-07-16 12:30:59 +0800

    update: new version pb_hrsystem added into main

--- lib/login/camera_page.dart
+++ lib/login/camera_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pb_hrsystem/login/ready_page.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 class CameraPage extends StatelessWidget {
   const CameraPage({super.key});
 
