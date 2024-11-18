4b4a993799be1dd19a63c2d9e1633e008f1f000a
llfoo119 <david.tech@subplace.com>
2024-11-18 15:59:45 +0800

    To add the material color in hive

---
+++ lib/models/material_color.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+
+class MaterialColorAdapter extends TypeAdapter<MaterialColor> {
+  @override
+  final int typeId = 100;
+
+  @override
+  MaterialColor read(BinaryReader reader) {
+    final int value = reader.readInt();
+    return Colors.primaries.firstWhere((color) => color.value == value);
+  }
+
+  @override
+  void write(BinaryWriter writer, MaterialColor obj) {
+    writer.writeInt(obj.value);
+  }
+}
